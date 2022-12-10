a7c25cc62c56be646fa005fe71f6dde2fcafe05e
Electrious_46 <80705688+royalfalcon1146@users.noreply.github.com>
2022-12-10 22:59:17 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,3 @@
+#Recursion: the repeated application of the same procedure to a smaller problem. Lets up tackle complex problems by reducing the problem to a simpler one.
+#In programming, recursion is a way of doing a repetitive task by having a function call itself.
+#Recursion in python can only happen 1000 times in each function when called.
