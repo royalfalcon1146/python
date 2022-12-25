872c03fa5eff13055fa97317e991227ffb886897
Electrious_46 <80705688+royalfalcon1146@users.noreply.github.com>
2022-12-25 08:18:12 +0300

    /

--- testing.py
+++ testing.py
@@ -1,1 +1,7 @@
 #This is for testing and not taking notes, here is the place I fight my challenges.
+myUser = {
+    "name": "Mohammed",
+    "age": 25,
+    "religion": "Islam"
+}
+print(myUser.name)
