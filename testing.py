74449f1d4b42564ea65f16cd300c52a45dbd5317
royalfalcon1146 <royalfalcon1146@gmail.com>
2022-12-25 12:07:23 +0300

    ....

--- testing.py
+++ testing.py
@@ -1,6 +1,8 @@
-myFile = {
-    "name": "testing", "size": 2000, "date": "25/12/2022"
-}
+def email_list(domains):
+	emails = []
+	for users in domains:
+	  for user in users:
+	    emails.append("{}{}".format(users, users[user]))
+	return(emails)
 
-for n in myFile:
-    print("{} : {}".format(n, myFile[n]))
+print(email_list({"gmail.com": ["clark.kent", "diana.prince", "peter.parker"], "yahoo.com": ["barbara.gordon", "jean.grey"], "hotmail.com": ["bruce.wayne"]}))
