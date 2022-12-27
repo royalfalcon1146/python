7c8845b24740dd92a0ffc8ac1f83eab195883413
royalfalcon1146 <royalfalcon1146@gmail.com>
2022-12-27 05:22:04 +0300

    did quiz

--- testing.py
+++ testing.py
@@ -1,13 +1,1 @@
-def groups_per_user(group_dictionary):
-	user_groups = {}
-	# Go through group_dictionary
-	for group in group_dictionary:
-		# Now go through the users in the group
-		for user in group_dictionary[group]:
-			
-
-	return(user_groups)
-
-print(groups_per_user({"local": ["admin", "userA"],
-		"public":  ["admin", "userB"],
-		"administrator": ["admin"] }))
+#Waiting for a worthy challenge
