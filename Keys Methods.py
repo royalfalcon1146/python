8b05b3b61bfd6e6362f7aeb03b8fa63084976475
royalfalcon1146 <royalfalcon1146@gmail.com>
2023-01-01 17:28:51 +0300

    added new things to key methods that will be useful in future Inshallah

---
+++ Keys Methods.py
@@ -0,0 +1,42 @@
+#https://pyautogui.readthedocs.io/en/latest/keyboard.html#keyboard-keys
+
+import pyautogui #the module that will be used
+
+#!The keys which can be pressed
+#!For mouse functions just search about the module itself
+
+theKeys = ['\t', '\n', '\r', ' ', '!', '"', '#', '$', '%', '&', "'", '(', 
+')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7',
+'8', '9', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`',
+'a', 'b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~',
+'accept', 'add', 'alt', 'altleft', 'altright', 'apps', 'backspace',
+'browserback', 'browserfavorites', 'browserforward', 'browserhome',
+'browserrefresh', 'browsersearch', 'browserstop', 'capslock', 'clear',
+'convert', 'ctrl', 'ctrlleft', 'ctrlright', 'decimal', 'del', 'delete',
+'divide', 'down', 'end', 'enter', 'esc', 'escape', 'execute', 'f1', 'f10',
+'f11', 'f12', 'f13', 'f14', 'f15', 'f16', 'f17', 'f18', 'f19', 'f2', 'f20',
+'f21', 'f22', 'f23', 'f24', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9',
+'final', 'fn', 'hanguel', 'hangul', 'hanja', 'help', 'home', 'insert', 'junja',
+'kana', 'kanji', 'launchapp1', 'launchapp2', 'launchmail',
+'launchmediaselect', 'left', 'modechange', 'multiply', 'nexttrack',
+'nonconvert', 'num0', 'num1', 'num2', 'num3', 'num4', 'num5', 'num6',
+'num7', 'num8', 'num9', 'numlock', 'pagedown', 'pageup', 'pause', 'pgdn',
+'pgup', 'playpause', 'prevtrack', 'print', 'printscreen', 'prntscrn',
+'prtsc', 'prtscr', 'return', 'right', 'scrolllock', 'select', 'separator',
+'shift', 'shiftleft', 'shiftright', 'sleep', 'space', 'stop', 'subtract', 'tab',
+'up', 'volumedown', 'volumemute', 'volumeup', 'win', 'winleft', 'winright', 'yen',
+'command', 'option', 'optionleft', 'optionright']
+
+pyautogui.write("Hello there") #the program will type out this by hitting the keys on the keyboard
+
+pyautogui.press("enter") #press is used when you want to do it one time, not holding it down or anything, just presses it
+
+with pyautogui.hold('shift'): #while the it holds on the "shift" key, it does the following after
+    pyautogui.press(['left', 'left', 'left']) 
+
+def key_event_handler(key): #this function here checks which key is pressed and does something about it, would be very useful
+    if key == 'a':
+        print("The 'a' key was pressed")
+
+pyautogui.onkeydown(key_event_handler) #call of the function
