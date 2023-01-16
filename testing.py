523eb6a92067d1ff022c52b402234dc268279942
royalfalcon1146 <royalfalcon1146@gmail.com>
2023-01-16 20:42:23 +0300

    File methods, gon destroy computers with this 😈😈😈

--- testing.py
+++ testing.py
@@ -1,17 +1,7 @@
-#Waiting for a challenge to come
-import tkinter as tk
-from tkinter import filedialog
+# There is no tomorrow!!!
+#! Waiting for a challenge!
+myFile = open(r"C:\Users\ALBARAA.DESKTOP-87EJ24R\Programming\Tests Folder\myTxtFile.txt")
+lines = []
+for line in myFile: lines.append(line.strip())
 
-# Create a GUI window
-root = tk.Tk()
-root.withdraw()
-
-# Open a file dialog and get the selected file's path
-file_path = filedialog.askopenfilename()
-
-# Open the file in read mode
-with open(file_path, 'r') as file:
-  # Read and process the file contents
-  for line in file:
-    # Do something with each line
-    pass
+print(lines)
