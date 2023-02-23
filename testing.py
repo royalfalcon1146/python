d3eb4d92978f50056e211c3ca19c44e149f0de9f
royalfalcon1146 <royalfalcon1146@gmail.com>
2023-02-23 15:32:31 +0300

    Auto Update from Windows

--- testing.py
+++ testing.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 #! THERE IS NO TOMORROW!!!
 #! DON'T WAIT FOR A CHANCE!!!
-print("hello world")
+
+from PIL import Image
+import subprocess
+import os
+
+# /opt/icons
+# /home/student-04-46d2e4258652/images
+
+my_process = subprocess.run("cd /home/student-04-46d2e4258652/images", capture_output=True)
